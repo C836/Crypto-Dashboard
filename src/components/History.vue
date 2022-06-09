80a1061d275669fbf01a5b9e891088c202c02e74
C836 <gl.sepiol@gmail.com>
2022-06-09 17:09:46 -0300

    requisição dos dados para histórico

--- src/components/History.vue
+++ src/components/History.vue
@@ -22,32 +22,34 @@ export default {
     <table class="text-left w-full h-auto max-w-5xl text-text bg-darker">
       <thead>
         <tr class="border-8 border-darker text-sm">
-          <th class="pl-3">Nome</th>
-
           <th class="text-right">Cap. Mercado</th>
 
           <th class="text-right">Preço</th>
 
-          <th class="pl-10">% 24H</th>
+          <th class="pl-10">24H</th>
         </tr>
       </thead>
-      <tbody
-        v-for="index in 30"
-        class="bg-darker border-separate"
-      >
-
+      <tbody v-for="index in 30" class="bg-darker border-separate">
         <tr>
-
           <td class="w-18 text-center text-xl font-medium">
-            <p>{{ history[0].prices[index][1] }}</p>
+            <p>R$ {{ history[0].market_caps[index][1].toFixed(4) }}</p>
+          </td>
+          <td class="w-18 text-center text-xl font-medium">
+            <p>R$ {{ history[0].prices[index][1].toFixed(4) }}</p>
           </td>
 
           <td class="w-18 text-center text-xl font-medium">
-            <p>{{ history[0].market_caps[index][1] }}</p>
+            <p>
+              R$ {{ (history[0].prices[index][1] - history[0].prices[index - 1][1]).toFixed(2) }}
+            </p>
           </td>
           
+          <td class="w-18 text-center text-xl font-medium">
+            <p>
+              {{ (((history[0].prices[index][1] - history[0].prices[index - 1][1]) / history[0].prices[index][1]) * 100).toFixed(2) }}%
+            </p>
+          </td>
         </tr>
-      
       </tbody>
     </table>
   </section>
