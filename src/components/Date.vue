3b2c44499a8c273694b0dcca89ce047dc9f3e905
C836 <gl.sepiol@gmail.com>
2022-06-10 06:47:13 -0300

    erro de renderização do input

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <input type="date" v-model="input" />
-    <p>{{ input }}</p>
     <button @click="handleInput">Get input field value</button>
   </div>
 </template>
@@ -10,9 +9,10 @@
 export default {
   data() {
     return {
-      date: new Date(),
+      input: null,
     };
   },
+  
   methods: {
     handleInput() {
       this.$emit("inputChange", this.input);
